c538f2c59721168110d415167e1421e9abe6b5dc
R.J. Hamilton <robert.hamilton@meijer.com>
2019-09-16 13:41:24 -0400

    Initial Commit

---
+++ ConversionCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConversionCalculator
+//
+//  Created by Hamilton, Robert on 9/16/19.
+//  Copyright © 2019 Hamilton, Robert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
